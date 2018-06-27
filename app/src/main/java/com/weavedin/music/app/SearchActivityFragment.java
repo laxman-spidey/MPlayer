7ddd00a2ed0a69f3adfada86e4795b88a6729834
laxman <mittu.thefire@gmail.com>
2018-06-28 00:19:52 +0530

    SplashSearchActivity - implemented

--- app/src/main/java/com/weavedin/music/app/SearchActivityFragment.java
+++ app/src/main/java/com/weavedin/music/app/SearchActivityFragment.java
@@ -1,10 +1,13 @@
 package com.weavedin.music.app;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,18 @@ public class SearchActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        EditText searchEditText = view.findViewById(R.id.searchEditText);
+        searchEditText.setFocusable(false);
+        CardView searchCard = view.findViewById(R.id.searchCard);
+        searchEditText.setOnClickListener(v->{
+            Intent intent = new Intent(getContext(), ItunesSearchActivity.class);
+            startActivity(intent);
+        });
+        searchCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), ItunesSearchActivity.class);
+            startActivity(intent);
+        });
+        return view;
     }
 }
