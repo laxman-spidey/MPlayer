2003d4c4ab006debe6503615ac1b748b783bfa86
laxman <mittu.thefire@gmail.com>
2018-06-28 22:10:06 +0530

    Favorites Activity added

---
+++ app/src/main/java/com/weavedin/music/app/FavoritesFragment.java
@@ -0,0 +1,38 @@
+package com.weavedin.music.app;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+
+public class FavoritesFragment extends Fragment {
+
+
+    public FavoritesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+        ImageButton backButton = view.findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> onBackButtonClicked(v));
+        return view;
+    }
+
+    public void onBackButtonClicked(View view) {
+        getActivity().finish();
+    }
+
+
+}
--- app/src/main/java/com/weavedin/music/app/ItunesSearchActivityFragment.java
+++ app/src/main/java/com/weavedin/music/app/ItunesSearchActivityFragment.java
@@ -1,5 +1,6 @@
 package com.weavedin.music.app;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +41,13 @@ public class ItunesSearchActivityFragment extends Fragment {
         searchBox.setOnClickListener(v->{
             searchBox.showDropDown();
         });
+        ImageButton favoritesButton = view.findViewById(R.id.favoritesIcon);
+        favoritesButton.setOnClickListener(v -> onFavoritesIconClicked(v));
         return view;
+    }
+
+    public void onFavoritesIconClicked(View view) {
+        Intent intent = new Intent(getContext(), FavoritesActivity.class);
+        startActivity(intent);
     }
 }
